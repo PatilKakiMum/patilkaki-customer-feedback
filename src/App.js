ec82483bd5b9e75b7df76e239212eb9891e33c7f
Vinit Patil <vinitp836@gmail.com>
2022-05-08 11:11:40 +0530

    question updates

--- src/App.js
+++ src/App.js
@@ -16,7 +16,38 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 
-
+var STEP_QUESTIONS = [
+  {
+    label: "Quality",
+    key: "qualityRating",
+    type: 'rating',
+    step: 1,
+  },
+  {
+    label: "Packaging",
+    key: "packagingRating",
+    type: 'rating',
+    step: 1,
+  },
+  {
+    label: "Delivery",
+    key: "deliveryRating",
+    type: 'rating',
+    step: 1,
+  },
+  {
+    label: "How Likely would you recommend us to a Friend",
+    key: "recommendationRating",
+    type: 'rating',
+    step: 3,
+  },
+  {
+    label: "Any Comments or any new product recommendations ?",
+    key: "comments",
+    type: 'text',
+    step: 3,
+  },
+];
 
 
 function App() {
@@ -24,15 +55,70 @@ function App() {
   const [step, setStep] = useState(0);
   const [loading, setLoading] = useState(true);
   const [orderData, setOrderData] = useState();
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
+
+
     axios.get(`https://ijapeekfjf.patilkaki.com/api/v2/feedback-order/${orderId}`).then(data => {
-      console.log({ data });
+
       setOrderData(data.data);
       setLoading(false);
+
+      if (STEP_QUESTIONS.length > 5)
+        return;
+      var usedSKUs = [];
+
+      data.data.order_items.map(item => {
+
+
+        if (!usedSKUs.includes(item.sku)) {
+          STEP_QUESTIONS.push({
+            label: item.name,
+            key: `product-${item.sku}`,
+            type: 'rating',
+            step: 2,
+          })
+
+          usedSKUs.push(item.sku);
+        }
+
+        return true;
+      })
+
+
+      if (!questions.length) {
+        setQuestions(STEP_QUESTIONS);
+      }
     })
 
-  }, [orderId])
+  }, [orderId, questions])
+
+  const setKeyValue = (key, value) => {
+    let tempQuestions = [...questions];
+    let questionIndex = questions.findIndex((obj => obj.key === key));
+    console.log({
+      questionIndex
+    });
+    tempQuestions[questionIndex].value = value;
+
+    setQuestions(tempQuestions);
+    console.log({ tempQuestions, key, value })
+  }
+
+  var AllInputsReceived = true;
+  var npsScore = 0;
+
+  questions.map(question => {
+    if (question.key === 'recommendationRating' && question.value)
+      npsScore = question.value;
+
+    if (question.step === step && question.type === 'rating') {
+      if (!question.value)
+        AllInputsReceived = false;
+    }
+    return true;
+  });
 
   const progressOutOf = step * 20;
   return (
@@ -69,110 +155,94 @@ function App() {
 
               </>}
 
-              {step === 1 && <>
-                <Typography variant="overline" component="div">
-                  Quality
-                </Typography>
-                <Rating
-                  name="simple-controlled"
-                  size='large'
-                  style={{ fontSize: 40, marginTop: 5, marginBottom: 35 }}
-                />
+              {questions.map(question => {
 
-                <Typography variant="overline" component="div">
-                  Packaging
-                </Typography>
-                <Rating
-                  name="simple-controlled"
-                  size='large'
-                  style={{ fontSize: 40, marginTop: 5, marginBottom: 35 }}
-                />
+                if (question.step === step) {
 
-                <Typography variant="overline" component="div"  >
-                  Delivery
-                </Typography>
-                <Rating
-                  name="simple-controlled"
-                  size='large'
-                  style={{ fontSize: 40, marginTop: 5, marginBottom: 35 }}
-                />
-              </>}
+                  if (question.type === 'rating') {
+                    return (
+                      <>
+                        <Typography variant="overline" component="div">
+                          {question.label}
+                        </Typography>
+                        <Rating
+                          name={question.key}
+                          value={question.value}
+                          max={(question.key === 'recommendationRating' ? 10 : 5)}
+                          onChange={(e, value) => setKeyValue(question.key, value)}
+                          size='large'
+                          style={{ fontSize: 40, marginTop: 5, marginBottom: 35 }}
+                        />
+                        <Typography>Value :{question.value}</Typography>
+                      </>
+                    )
+                  } else {
+                    return (
+                      <>
+                        <Typography variant="overline" component="div">
+                          {question.label}
+                        </Typography>
+                        <TextField
+                          id="outlined-multiline-static"
+                          name={question.key}
+                          value={question.value}
+                          onChange={(e, value) => setKeyValue(question.key, value)}
+                          multiline
+                          rows={4}
+                          style={{ marginTop: 5, marginBottom: 35, width: '100%' }}
+                        />
+                      </>
+                    )
+                  }
+                }
 
-              {step === 2 && <>
-                {orderData.order_items.map(item => <>
-                  <Typography variant="overline" component="div">
-                    {item.name}
-                  </Typography>
-                  <Rating
-                    name={item.sku}
-                    size='large'
-                    style={{ fontSize: 40, marginTop: 5, marginBottom: 35 }}
-                  />
-                </>)}
+                return false;
 
+              })}
 
-              </>}
-
-              {step === 3 && <>
-                <Typography variant="overline" component="div">
-                  How Likely would you recommend us to a Friend
-                </Typography>
-                <Rating
-                  name="simple-controlled"
-                  size='large'
-                  max={10}
-                  style={{ fontSize: 40, marginTop: 5, marginBottom: 35 }}
-                />
-
-
-                <Typography variant="overline" component="div">
-                  Any Comments or any new product recommendations ?
-                </Typography>
-                <TextField
-                  id="outlined-multiline-static"
-                  multiline
-                  rows={4}
-                  style={{ marginTop: 5, marginBottom: 35, width: '100%' }}
-                />
-
-              </>}
 
               {step === 4 && <>
                 <Typography>
                   We're genuinely thankful for your valuable feedback. As a customer first brand, it really helps us understand how well we are able to serve our PatilKaki community. Here are the next steps-
                 </Typography>
 
-                <hr style={{ marginTop: 20, marginBottom: 50 }} />
+                {npsScore > 7 && <>
+                  <hr style={{ marginTop: 20, marginBottom: 50 }} />
 
-                <Grid container style={{ backgroundColor: "#ffede6", cursor: "pointer", padding: 5, paddingBottom: 10, borderRadius: 10, marginBottom: 30 }} >
-                  <Grid item xs={2} style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
-                    <DiscountIcon style={{ fontSize: 30 }} />
-                  </Grid>
-                  <Grid item xs={10} style={{ textAlign: "left" }}  >
-                    <Typography variant='h6' > Refer & Save </Typography>
-                    <Typography variant='subtitle2'> Give 20% & Get 25% </Typography>
-                  </Grid>
-                </Grid>
+                  <a href="https://patilkaki.com/refer" target='_blank' rel="noreferrer" style={{ color: '#333', textDecoration: "none" }} >
 
+                    <Grid container style={{ backgroundColor: "#ffede6", cursor: "pointer", padding: 5, paddingBottom: 10, borderRadius: 10, marginBottom: 30 }} >
+                      <Grid item xs={2} style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+                        <DiscountIcon style={{ fontSize: 30 }} />
+                      </Grid>
+                      <Grid item xs={10} style={{ textAlign: "left" }}  >
+                        <Typography variant='h6' > Refer & Save </Typography>
+                        <Typography variant='subtitle2'> Give 20% & Get 25% </Typography>
+                      </Grid>
+                    </Grid>
 
-                <Grid container style={{ backgroundColor: "#ffede6", cursor: "pointer", padding: 5, paddingBottom: 10, borderRadius: 10, marginBottom: 10 }} >
-                  <Grid item xs={2} style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
-                    <ReviewsIcon style={{ fontSize: 30 }} />
-                  </Grid>
-                  <Grid item xs={10} style={{ textAlign: "left" }}  >
-                    <Typography variant='h6' > Write a Social Review </Typography>
-                    <Typography variant='subtitle2'> Get a FLAT 20% Discount Code </Typography>
-                  </Grid>
-                </Grid>
+                  </a>
 
+                  <a href="https://linktr.ee/patilkaki" target='_blank' rel="noreferrer" style={{ color: '#333', textDecoration: "none" }} >
+                    <Grid container style={{ backgroundColor: "#ffede6", cursor: "pointer", padding: 5, paddingBottom: 10, borderRadius: 10, marginBottom: 10 }} >
+                      <Grid item xs={2} style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+                        <ReviewsIcon style={{ fontSize: 30 }} />
+                      </Grid>
+                      <Grid item xs={10} style={{ textAlign: "left" }}  >
+                        <Typography variant='h6' > Write a Social Review </Typography>
+                        <Typography variant='subtitle2'> Get a FLAT 20% Discount Code </Typography>
+                      </Grid>
+                    </Grid>
+                  </a>
 
+                </>}
 
               </>}
             </div>
 
           </CardContent>
           <CardActions>
-            {step < 4 && <Button fullWidth variant='contained' onClick={() => setStep(step + 1)} > NEXT </Button>}
+            {step < 4 && <Button fullWidth disabled={!AllInputsReceived} variant='contained' onClick={() => setStep(step + 1)} > NEXT </Button>}
           </CardActions>
         </>}
         {loading && <>
